bc37725977074a0fcb298d32bafab54b778a35b4
pedadakirankishor1 <140369578+pedadakirankishor1@users.noreply.github.com>
2023-08-17 15:44:31 +0530

    Add files via upload

---
+++ List1.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class List1 {
+
+	public static void main(String[] args) {
+		List<Integer> list2= Arrays.asList(2,9,83,76,54,90);
+		Collections.sort(list2);
+		System.out.println(list2);
+		// TODO Auto-generated method stub
+
+	}
+
+}
